95ba846f60b6c4c836f455f7997f0590ab06695d
Adityo-dev <computerscience01770@gmail.com>
2025-12-06 00:44:52 +0600

    Add, Delete and completed [DONE]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Todos from "@/components/Todos";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full px-4 sm:px-6 md:px-10 pt-24 flex flex-col items-center gap-10">
+    <main className="min-h-screen w-full px-4 sm:px-6 md:px-10 mt-12 md:pt-24 flex flex-col items-center gap-10">
       <h2 className="text-2xl sm:text-3xl md:text-4xl uppercase font-bold text-center">
         <span className="text-orange-500">todo</span> next js + typescript
       </h2>
